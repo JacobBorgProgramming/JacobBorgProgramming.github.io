42404992cc3d46e3d09673576664fccba010e80c
Jacob B <pokeborg3@gmail.com>
2019-06-29 11:43:54 -0700

    made some improvements

---
+++ atomic.js
@@ -0,0 +1,23 @@
+//Atomic Core 1.5.3
+var r1 = "Z" + Math.floor(Math.random() * 1001);
+var r2 = "Q" + Math.floor(Math.random() * 101);
+var r3 = "k" + Math.floor(Math.random() * 1001);
+var r4 = "p" + Math.floor(Math.random() * 101);
+var r5 = "Q" + Math.floor(Math.random() * 1001);
+var r6 = "L" + Math.floor(Math.random() * 101);
+var rr1 = Math.floor(Math.random() * 21);
+var d = new Date();
+
+var rr2 = r1 + r6
+var rd = Date.length
+
+var rr3 = r2 + r4
+var rrr1 = rr1 + rr2
+var result = r4 + "-" + r3 + "-" + r2 + "-" + r5 + "-" + r1
+var rsln1 = result.length
+var rsln2 = rrr1.length
+var sc = screen.availHeight+screen.availWidth + rr1
+var rrr2= sc + rsln1
+function cA1N(){
+window.alert(result + "-" + rsln1 + "-" + rrr2);
+}
