3c7aa913f5277b7b66d9c9ce1a1f7129b1f90f2e
Jacob B <pokeborg3@gmail.com>
2019-07-18 20:54:54 -0700

    updated atomic

--- atomic.js
+++ atomic.js
@@ -1,13 +1,13 @@
-//Atomic Core 1.5.3
+//Atomic Core 1.5.5
 var r1 = "Z" + Math.floor(Math.random() * 1001);
 var r2 = "Q" + Math.floor(Math.random() * 101);
 var r3 = "k" + Math.floor(Math.random() * 1001);
 var r4 = "p" + Math.floor(Math.random() * 101);
 var r5 = "Q" + Math.floor(Math.random() * 1001);
 var r6 = "L" + Math.floor(Math.random() * 101);
-var a1 = "w"
+var a1 = "cA"
 var a2 ="X"
-var rr1 = Math.floor(Math.random() * 21);
+var rr1 = Math.floor(Math.random() * 33);
 var d = new Date();
 
 var rr2 = r1 + r6
@@ -15,11 +15,24 @@ var rd = Date.length
 
 var rr3 = r2 + r4
 var rrr1 = rr1 + rr2
-var result = r4 + "-" + r3 + "-" + r2 + "-" + r5 + "-" + r1
-var rsln1 = result.length
+var result = r4 + "-" + r3 + "-" + r2 + "-" + r5 + "-" + r6
+var rsln1 = result.length + r1
 var rsln2 = rrr1.length
 var sc = screen.availHeight+screen.availWidth + rr1
 var rrr2= sc + rsln1
 function cA1N(){
-window.alert("cA1.5.4:    "+result + "-" + rsln1 + "-" + a1 + rr1 + a2);
+window.alert("cA1.5.5:    "+result + "-" + rsln1 + "-" + a1 + rr1 + a2);
+}
+
+//Simple Atomic Core
+var sca1 = Math.floor(Math.random() * 101);
+var sca2 = Math.floor(Math.random() * 1001);
+var sca4 = Math.floor(Math.random() * 101);
+var sca5 = Math.floor(Math.random() * 1001);
+var sca3 = "scA"
+var resultsca = sca1 + sca2 + sca3 + sca4 + sca5
+
+
+function scA(){
+window.alert("scA1.0:    "+resultsca);
 }
