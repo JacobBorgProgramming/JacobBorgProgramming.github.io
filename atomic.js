fec949c28f1aa3c0ccea7be49ef9e22ac5621cde
Jacob B <pokeborg3@gmail.com>
2019-07-22 19:50:32 -0700

    Updated Atomic

--- atomic.js
+++ atomic.js
@@ -1,18 +1,23 @@
-//Atomic Core 1.5.5
+//Atomic Core 1.5.6
 var r1 = "Z" + Math.floor(Math.random() * 1001);
 var r2 = "Q" + Math.floor(Math.random() * 101);
 var r3 = "k" + Math.floor(Math.random() * 1001);
 var r4 = "p" + Math.floor(Math.random() * 101);
-var r5 = "Q" + Math.floor(Math.random() * 1001);
+var r5 = "K" + Math.floor(Math.random() * 1001);
 var r6 = "L" + Math.floor(Math.random() * 101);
 var a1 = "cA"
 var a2 ="X"
+var r = Math.floor(Math.random() * 89);
+var q = Math.floor(Math.random() * 3600);
+var k = Math.floor(Math.random() * 36000);
+var mcc1 = Math.asinh(r+q);
+var mcc2 = Math.trunc(mcc1)
+var mcc3 = Math.trunc(k)
 var rr1 = Math.floor(Math.random() * 33);
 var d = new Date();
 
 var rr2 = r1 + r6
 var rd = Date.length
-
 var rr3 = r2 + r4
 var rrr1 = rr1 + rr2
 var result = r4 + "-" + r3 + "-" + r2 + "-" + r5 + "-" + r6
@@ -21,18 +26,9 @@ var rsln2 = rrr1.length
 var sc = screen.availHeight+screen.availWidth + rr1
 var rrr2= sc + rsln1
 function cA1N(){
-window.alert("cA1.5.5:    "+result + "-" + rsln1 + "-" + a1 + rr1 + a2);
+window.alert("cA1.5.6:    "+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3);
 }
-
-//Simple Atomic Core
-var sca1 = Math.floor(Math.random() * 101);
-var sca2 = Math.floor(Math.random() * 1001);
-var sca4 = Math.floor(Math.random() * 101);
-var sca5 = Math.floor(Math.random() * 1001);
-var sca3 = "scA"
-var resultsca = sca1 + sca2 + sca3 + sca4 + sca5
-
-
-function scA(){
-window.alert("scA1.0:    "+resultsca);
-}
+console.log('cA: 1.5.6')
+console.log('Your Atomic Password is:'+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3)
+document.getElementById("jsC1").innerHTML = result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3;
+document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
