28276df60227946cb484aff6355e826b3a457fa4
Jacob B <pokeborg3@gmail.com>
2019-07-25 10:29:15 -0700

    Updates
    ATOMIC has been updated to 1.5.7
    
    Improved password generation

--- atomic.js
+++ atomic.js
@@ -1,10 +1,10 @@
-//Atomic Core 1.5.6
-var r1 = "Z" + Math.floor(Math.random() * 1001);
-var r2 = "Q" + Math.floor(Math.random() * 101);
+//Atomic Core 1.5.7
+var r1 = "B" + Math.floor(Math.random() * 1001);
+var r2 = "Y" + Math.floor(Math.random() * 101);
 var r3 = "k" + Math.floor(Math.random() * 1001);
-var r4 = "p" + Math.floor(Math.random() * 101);
-var r5 = "K" + Math.floor(Math.random() * 1001);
-var r6 = "L" + Math.floor(Math.random() * 101);
+var r4 = "Q" + Math.floor(Math.random() * 101);
+var r5 = "JJ" + Math.floor(Math.random() * 1001);
+var r6 = "X" + Math.floor(Math.random() * 101);
 var a1 = "cA"
 var a2 ="X"
 var r = Math.floor(Math.random() * 89);
@@ -20,15 +20,15 @@ var rr2 = r1 + r6
 var rd = Date.length
 var rr3 = r2 + r4
 var rrr1 = rr1 + rr2
-var result = r4 + "-" + r3 + "-" + r2 + "-" + r5 + "-" + r6
+var result = r4 + "-" + r3 + "-" + r5 + "-" + r6
 var rsln1 = result.length + r1
 var rsln2 = rrr1.length
 var sc = screen.availHeight+screen.availWidth + rr1
 var rrr2= sc + rsln1
 function cA1N(){
-window.alert("cA1.5.6:    "+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3);
+window.alert("cA1.5.7:    "+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3);
 }
-console.log('cA: 1.5.6')
+console.log('cA: 1.5.7')
 console.log('Your Atomic Password is:'+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3)
 document.getElementById("jsC1").innerHTML = result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3;
 document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
