654be29e43a04d8167f9c53a97dcb5735eadcfb1
Jacob B <pokeborg3@gmail.com>
2019-07-29 17:34:30 -0700

    Update atomic.js

--- atomic.js
+++ atomic.js
@@ -10,7 +10,7 @@ var a2 ="X"
 var r = Math.floor(Math.random() * 89);
 var q = Math.floor(Math.random() * 3600);
 var k = Math.floor(Math.random() * 36000);
-var mcc1 = Math.asinh(r+q);
+var mcc1 = Math.asinh(r+q+a2);
 var mcc2 = Math.trunc(mcc1)
 var mcc3 = Math.trunc(k)
 var rr1 = Math.floor(Math.random() * 33);
@@ -20,15 +20,15 @@ var rr2 = r1 + r6
 var rd = Date.length
 var rr3 = r2 + r4
 var rrr1 = rr1 + rr2
-var result = r4 + "-" + r3 + "-" + r5 + "-" + r6
+var result = r1 + "-" + r3 + "-" + r6
 var rsln1 = result.length + r1
 var rsln2 = rrr1.length
 var sc = screen.availHeight+screen.availWidth + rr1
 var rrr2= sc + rsln1
 function cA1N(){
-window.alert("cA1.5.7:    "+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3);
+window.alert("cA1.5.8:    "+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3);
 }
-console.log('cA: 1.5.7')
-console.log('Your Atomic Password is:'+result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3)
-document.getElementById("jsC1").innerHTML = result + "-" + rsln1 + "-" + rr1 + '-' + mcc2 + mcc3;
+console.log('cA: 1.5.8')
+console.log('Your Atomic Password is:'+result + "-" + rsln1 + "-" + '-' + mcc2 + mcc3)
+document.getElementById("jsC1").innerHTML = result + "-" + rsln1 + "-" + '-' + mcc2 + mcc3;
 document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
