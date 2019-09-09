1d67f94f9d31baf2b84c9c14c55a4c039c67116d
JacobBorgProgramming <48691704+JacobBorgProgramming@users.noreply.github.com>
2019-09-09 13:29:39 -0700

    updates

--- atomic.js
+++ atomic.js
@@ -5,9 +5,10 @@ var r3 = "k" + Math.floor(Math.random() * 1001);
 var r4 = "Q" + Math.floor(Math.random() * 101);
 var r5 = "JJ" + Math.floor(Math.random() * 1001);
 var r6 = "X" + Math.floor(Math.random() * 101);
-var a1 = "cA"
+var a1 = "cA!@@"
 var a2 ="X"
 var r = Math.floor(Math.random() * 89);
+var sr1 = Math.floor(Math.random() * 11);
 var q = Math.floor(Math.random() * 3600);
 var k = Math.floor(Math.random() * 36000);
 var mcc1 = Math.asinh(r+q);
@@ -20,15 +21,15 @@ var rr2 = r1 + r6
 var rd = Date.length
 var rr3 = r2 + r4
 var rrr1 = rr1 + rr2
-var result = r1 + "-" + r3 + "-" + r6
+var result = a1 + "-" + r6 + "-" + r4
 var rsln1 = result.length + r1
 var rsln2 = rrr1.length
-var sc = screen.availHeight+screen.availWidth + rr1
+var sc = screen.availHeight+screen.availWidth * sr1
 var rrr2= sc + rsln1
 function cA1N(){
-window.alert("cA1.5.9:    "+result + "-" + rsln1 + '-' + mcc2 + rr1 + '-' + sc);
+window.alert("cA1.6.0:    "+result + "-" + rsln1 + '-' + r5 + '-' + sc);
 }
-console.log('cA: 1.5.9')
-console.log('Your Atomic Password is:'+result + "-" + rsln1 + '-' + mcc2 + rr1 + '-' + sc)
-document.getElementById("jsC1").innerHTML = result + "-" + rsln1 + '-' + mcc2 + rr1 + '-' + sc;
+console.log('cA: 1.6.0')
+console.log('Your Atomic Password is:'+result + "-" + rsln1 + '-' + r5 + '-' + sc)
+document.getElementById("jsC1").innerHTML = result + "-" + rsln1 + '-' + r5 + '-' + sc;
 document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
