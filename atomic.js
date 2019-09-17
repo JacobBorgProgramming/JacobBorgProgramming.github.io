8513fbb948941843e17c4cbf8fe0fe2978dd1a29
Jacob B <pokeborg3@gmail.com>
2019-09-17 10:16:52 -0700

    CSS updates
    Updated to use JazzBlocks Divided

--- atomic.js
+++ atomic.js
@@ -1,4 +1,4 @@
-//Atomic Core 1.5.8
+//Atomic Core 1.6.0
 var r1 = "b" + Math.floor(Math.random() * 1001);
 var r2 = "Y" + Math.floor(Math.random() * 101);
 var r3 = "k" + Math.floor(Math.random() * 1001);
