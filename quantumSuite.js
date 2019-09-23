2ece0460591b3d6ceef43364d04e425a25630d89
Jacob Borg <jacob.d.borg@gmail.com>
2019-09-23 14:45:32 -0700

    updates

---
+++ quantumSuite.js
@@ -0,0 +1,11 @@
+if (navigator.cookieEnabled === false){
+  console.log('Cookies are disabled')
+  document.getElementById("jsC1").innerHTML = "Your cookies are disabled.";
+  document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
+}
+if (navigator.cookieEnabled === true){
+  document.getElementById("jsC1").innerHTML = "Your cookies are enabled.";
+  document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
+  console.log('Cookies are enabled')
+}
+console.log('JacobBorgProgramming 2019: Cookie Checker')
